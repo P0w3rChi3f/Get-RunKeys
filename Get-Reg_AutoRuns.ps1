1efa9b720f2f6786170662d686d55d7441480566
P0w3rChi3f <honeycutt.james@gmail.com>
2022-02-07 09:24:19 -0500

    Initial Upload

---
+++ Get-Reg_AutoRuns.ps1
@@ -0,0 +1,31 @@
+﻿$creds = Get-Credential
+
+$Range = @("jea-dc", "jea-demoSVR")
+
+foreach ($ip in $range) {
+
+    Invoke-Command -ComputerName $ip -Credential $creds -ScriptBlock {
+
+        
+        if (Test-Path HKLM:\Software\Microsoft\Windows\CurrentVersion\Run) {         
+            Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Run} # end IF
+        else {Write-Host "HKLM Run key does not exist."}
+        
+        if (Test-Path HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce) { 
+        Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce} #end IF
+        else {Write-Host "HKLM RunOnce key does not exist."}
+
+        if (Test-Path HKLM:\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\Run){
+        Get-ItemProperty HKLM:\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\Run} #end IF
+        else {Write-Host "DEFAULT Run key does not exist."}
+
+        if(Test-Path HKLM:\Software\Wow6432node\Microsoft\Windows\CurrentVersion\Run){
+        Get-ItemProperty HKLM:\Software\Wow6432node\Microsoft\Windows\CurrentVersion\Run}
+        else {Write-Host "HKLM Wow6432 Run key does not exist."} #end IF
+
+        if(test-path HKLM:\Software\Wow6432node\Microsoft\Windows\CurrentVersion\RunOnce){
+        Get-ItemProperty HKLM:\Software\Wow6432node\Microsoft\Windows\CurrentVersion\RunOnce}
+        else {Write-Host "HKLM Wow6432 RunOnce key does not exist."} #end IF
+
+    } # Closes Script Block
+} # Closes Loop
